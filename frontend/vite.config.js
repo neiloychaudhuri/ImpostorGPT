8a89717acb407ddf007c6f677c79d3598f1996a7
neiloyc <neiloychaudhuri@gmail.com>
2026-01-17 18:38:33 -0500

    Change build output to root dist directory to match Vercel expectations

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: '../dist',
     emptyOutDir: true,
     minify: 'esbuild',
     chunkSizeWarningLimit: 1000
